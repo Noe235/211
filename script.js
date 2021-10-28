23d9a554a35cfef9f97d7804235889da85512c73
Noe235 <sannoemie@outlook.com>
2021-10-28 14:15:35 -0400

    week7

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const selenSike = new Audio("assets/sound/ONE_TWO.mp3");
 let lastTitle = "changing title";
 let lastImage = "assets/image/bearselen.png";
-let imagePosititon = 0;
+let imageHorizontal = 1;
+let imageVertical = 1;
 
 //for moving picture, every 20 millis
 //setInterval (movePicture,20);
+document.addEventListener("keydown", keyboard)
 
 function myFunction() {
   selenSike.play();
@@ -30,3 +32,23 @@ function myFunction() {
 //   imagePosititon += 3;
 //   document,getElementById(`dynamicPicture`).style.right += "px";
 // }
+function keyboard(event) {
+
+  if (event.keyCode === 38) {
+    imageVertical = imageVertical + 3;
+    console.log("working")
+
+  }
+  if (event.keyCode === 40) {
+    imageVertical -= 3;
+  }
+  if (event.keyCode === 37) {
+    imageHorizontal += 3;
+  }
+  if (event.keyCode === 39) {
+    imageHorizontal -= 3;
+  }
+  document.getElementById("dynamicPicture").style.right = imageHorizontal + "px";
+  document.getElementById("dynamicPicture").style.bottom = imageVertical + "px";
+  // window.alert("Hello World");
+}
